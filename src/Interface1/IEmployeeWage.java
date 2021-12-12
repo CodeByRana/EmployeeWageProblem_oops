d8db4b1ea0b4c6ee2460fe797d2690c307bd5c5a
deepakphpweb <dipakrana2760@gmail.com>
2021-12-12 22:39:35 +0530

    Added : Added new interface and impliments there methods

---
+++ src/Interface1/IEmployeeWage.java
@@ -0,0 +1,11 @@
+package Interface1;
+
+public class IEmployeeWage {
+    void addCompanyEmpWage(String companyName, int ratePerHrs, int totalHours, int daysPerMonth) {
+
+    }
+
+    void empWageArray() {
+
+    }
+}
---
+++ src/model/CompanyEmpWage.java
@@ -0,0 +1,25 @@
+package model;
+
+public class CompanyEmpWage {
+    String compName;
+    public int RATE_PER_HOUR;
+    public int MAX_WORKING_HOURS;
+    public int workingDayInMonth;
+
+    public CompanyEmpWage(String compName, int RATE_PER_HOUR, int MAX_WORKING_HOURS, int workingDayInMonth) {
+        this.compName = compName;
+        this.RATE_PER_HOUR = RATE_PER_HOUR;
+        this.MAX_WORKING_HOURS = MAX_WORKING_HOURS;
+        this.workingDayInMonth = workingDayInMonth;
+    }
+
+    @Override
+    public String toString() {
+        return "CompanyEmpWage{" +
+                "compName='" + compName + '\'' +
+                ", RATE_PER_HOUR=" + RATE_PER_HOUR +
+                ", MAX_WORKING_HOURS=" + MAX_WORKING_HOURS +
+                ", workingDayInMonth=" + workingDayInMonth +
+                '}';
+    }
+}
