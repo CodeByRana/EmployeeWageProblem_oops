5ff8056ff0fdcbfeec536902d68628e072519341
deepakphpweb <dipakrana2760@gmail.com>
2021-12-12 22:18:15 +0530

    Added : Employee wage for multiple company

--- src/com/bridge/empwage/EmployeeWage.java
+++ src/com/bridge/empwage/EmployeeWage.java
@@ -1,20 +1,28 @@
 package com.bridge.empwage;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class EmployeeWage {
     int status;
-    int RATE_PER_HOUR = 20;
-    int MAX_WORKING_HOURS = 100;
-    int wage = 0;
+    String compName;
+    int RATE_PER_HOUR;
+    int MAX_WORKING_HOURS;
     final int PART_TIME = 2;
     final int FULL_TIME = 1;
     int empHours = 0;
     int totalMonthlyWage = 0;
-    int workingDayInMonth = 20;
+    int workingDayInMonth;
     int totalWorkingHours = 0;
 
+    EmployeeWage(String name, int workingRate, int workingHoursInMonth, int workingDayInMonth){
+        this.compName=name;
+        this.RATE_PER_HOUR=workingRate;
+        this.MAX_WORKING_HOURS=workingHoursInMonth;
+        this.workingDayInMonth=workingDayInMonth;
+    }
     //Check whether the employee is present or not
     private int checkStatus() {
-
         this.status = ((int) (Math.random() * 10) % 3);
         switch (status) {
             case FULL_TIME:
@@ -44,14 +52,21 @@ public class EmployeeWage {
     }
 
     private void printOutput() {
-        System.out.println("Total Working Hours in month is : " + totalWorkingHours);
-        System.out.println("Total employee wage in month : " + totalMonthlyWage);
+        System.out.println("Total employee wage for Company "+compName+" is "+totalMonthlyWage);
+//        System.out.println("Total Working Hours in month is : " + totalWorkingHours);
+//        System.out.println("Total employee wage in month : " + totalMonthlyWage);
     }
 
     public static void main(String[] args) {
-        EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.calculateDailyWage();
-        employeeWage.wageForMonth();
-        employeeWage.printOutput();
+        //for the working hour rate 20, working hour in month- 100 working hour in month 20
+
+        EmployeeWage abc= new EmployeeWage("ABC",20,100,20);
+        abc.calculateDailyWage();
+        abc.wageForMonth();
+        abc.printOutput();
+        EmployeeWage apple=new EmployeeWage("Apple",25,120,25);
+        apple.calculateDailyWage();
+        apple.wageForMonth();
+        apple.printOutput();
     }
 }
